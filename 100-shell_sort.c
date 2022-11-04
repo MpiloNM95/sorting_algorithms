4c052bec51281d2518fef5c749d98dc0b1616c3f
MpiloNM95 <mpilomafu95@gmail.com>
2022-11-04 07:13:10 -0700

    done

---
+++ 100-shell_sort.c
@@ -0,0 +1,20 @@
+#include "sort.h"
+/**
+ * shell_sort - sorts an array of integers in ascending order using a shell
+ * sort algorithm, with gap sizes determined by a decreasing Knuth seqeuence
+ * @array: array of integers to be sorted
+ * @size: amount of elements in array
+ */
+void shell_sort(int *array, size_t size)
+{
+	int i, j, gap, n, Knuth_max, temp;
+
+	if (!array || size < 2)
+		return;
+
+	n = (int)size;
+	for (gap = 1; gap < n; gap = (gap * 3) + 1)
+	{
+		Knuth_max = gap;
+	}
+}
