409d829a8354bc794bc2ea9e74a9d8cc59e3a3a1
MpiloNM95 <mpilomafu95@gmail.com>
2022-11-05 05:55:32 -0700

    done

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -20,3 +20,25 @@ void swap1(listint_t **list, listint_t *head, listint_t *aux)
 	head->prev = aux;
 	print_list(*list);
 }
+/**
+ * swap2 - swaps nodes from right to left
+ * @list: pointer to list
+ * @head: pointer to head node
+ * @aux: auxiliar pointer
+ * Return: no return
+ */
+void swap2(listint_t **list, listint_t *head, listint_t *aux)
+{
+	aux = head->prev;
+	aux->next->prev = aux->prev;
+	if (aux->prev)
+		aux->prev->next = aux->next;
+	else
+		*list = aux->next;
+	aux->prev = aux->next;
+	aux->next = aux->next->next;
+	aux->prev->next = aux;
+	if (aux->next)
+		aux->next->prev = aux;
+	print_list(*list);
+}
