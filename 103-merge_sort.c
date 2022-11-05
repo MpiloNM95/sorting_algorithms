35679c9c10d9f2c504bd9f344e8ff485e48dd8a0
MpiloNM95 <mpilomafu95@gmail.com>
2022-11-05 07:28:14 -0700

    done

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -19,4 +19,20 @@ void merge(int *array, int *l, int *r, size_t size)
 	print_array(l, size_l);
 	printf("[right]: ");
 	print_array(r, size_r);
+
+	while (i < size_l && j < size_r)
+	{
+		if (l[i] < r[j])
+			array[k++] = l[i++];
+		else
+			array[k++] = r[j++];
+	}
+
+	while (i < size_l)
+		array[k++] = l[i++];
+
+	while (j < size_r)
+		array[k++] = r[j++];
+	printf("[Done]: ");
+	print_array(array, size);
 }
