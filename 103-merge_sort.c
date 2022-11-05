bb38b3ba946bcadb29f74540433766b484d9ccb1
MpiloNM95 <mpilomafu95@gmail.com>
2022-11-05 08:20:36 -0700

    done

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -33,3 +33,41 @@ void CopyArray(int *source, int iBeg, int iEnd, int *dest)
 	for (i = iBeg; i < iEnd; i++)
 		dest[i] = source[i];
 }
+
+/**
+ * TopDownMerge - sorts subsections ("runs") of source[] by ascending value
+ * @source: array of values to be sorted
+ * @iBeg: left run starting index value
+ * @iMid: right run starting index value
+ * @iEnd: right run ending index value
+ * @dest: array to store sorted integers
+ */
+void TopDownMerge(int *source, int iBeg, int iMid, int iEnd, int *dest)
+{
+	int i, j, k;
+
+	i = iBeg, j = iMid;
+
+	printf("Merging...\n");
+	printf("[left]: ");
+	PrintArray(source, iBeg, iMid);
+	printf("[right]: ");
+	PrintArray(source, iMid, iEnd);
+	/* While there are elements in the left or right runs... */
+	for (k = iBeg; k < iEnd; k++)
+	{
+		/* If left run head exists and is <= existing right run head */
+		if (i < iMid && (j >= iEnd || source[i] <= source[j]))
+		{
+			dest[k] = source[i];
+			i++;
+		}
+		else
+		{
+			dest[k] = source[j];
+			j++;
+		}
+	}
+	printf("[Done]: ");
+	PrintArray(dest, iBeg, iEnd);
+}
