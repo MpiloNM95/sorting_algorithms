a48466712165b7a12bbe93bcbc7f348ebbea01e7
MpiloNM95 <mpilomafu95@gmail.com>
2022-11-05 08:01:03 -0700

    done

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -18,3 +18,18 @@ void PrintArray(int *array, int iBeg, int iEnd)
 		else
 			printf("%i\n", array[i]);
 }
+
+/**
+ * CopyArray - simple 1 for 1 copy of source[] to dest[]
+ * @source: array of values to be sorted
+ * @iBeg: starting index value
+ * @iEnd: ending index value
+ * @dest: array to store sorted integers
+ */
+void CopyArray(int *source, int iBeg, int iEnd, int *dest)
+{
+	int i;
+
+	for (i = iBeg; i < iEnd; i++)
+		dest[i] = source[i];
+}
